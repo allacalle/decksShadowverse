2d541d3393e51f0ed4ceb6a4c47080f17598778b
Alfonso <allacalle@gmail.com>
2023-09-27 18:45:42 +0200

    La lie parda con los iconos, aqui no hay inos pero si que funciona

--- decks/forms.py
+++ decks/forms.py
@@ -1,12 +1,13 @@
-# forms.py
 from django import forms
 from .models import Deck, ShadowverseClass
 
 class DeckForm(forms.ModelForm):
-    clase_deck = forms.ChoiceField(
-        choices=[(clase.name_class, clase.name_class) for clase in ShadowverseClass.objects.all()],
+    clase_deck = forms.ModelChoiceField(
+        queryset=ShadowverseClass.objects.all(),
+        empty_label='Seleccionar una clase',
         widget=forms.RadioSelect,
-        label='Clase del Mazo'
+        label='Clase del Mazo',
+        to_field_name='name_class'  # Utilizar el nombre de la clase como valor
     )
 
     class Meta:
